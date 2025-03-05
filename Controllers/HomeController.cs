eb352e831dbe38058272a100506851d9442fc3bd
Mitchell Thomas <mthomas60@my.wctc.edu>
2025-03-05 16:39:27 -0600

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+public class HomeController(DataContext db) : Controller
+{
+    // this controller depends on the DataContext
+    private readonly DataContext _dataContext = db;
+
+    public IActionResult Index() => View(_dataContext.Categories.OrderBy(b => b.CategoryName));
+}
